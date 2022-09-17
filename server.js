5a06e5bd18d0e815cad16e9702dda7a5e82071ce
suraj-chakraborty <surajchakraborty211@gmail.com>
2022-09-17 11:23:02 +0000

    error fix 3.0

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ import { PrismaClient } from "@prisma/client";
 import cookie from "@fastify/cookie";
 import path from "path";
 import serveStatic from "@fastify/static";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = fastify({ logger: true });
